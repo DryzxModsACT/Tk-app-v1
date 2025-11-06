e1cd7d3512dd173906bc4368636cab2df62f1ef4
DryzxModsACT <andrygege450@gmail.com>
2025-11-06 17:37:28 +0700

    Update config.js

--- database/config.js
+++ database/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  tokens: "8371501056:AAF3Af42cBFsPHaE3EZXYYvtspiH3j1JmxE",  // Ubah Jadi Token Bot Mu !!!
+  tokens: "8371501056:AAEolBwcCChp41mxLQWi6DTIgxw-SJIVYWA",  // Ubah Jadi Token Bot Mu !!!
   owner: "8157933109", // Ubah Jadi Id Mu !!!
   port: "1466", // Ubah Jadi Port Panel Mu !!!
   ipvps: "-" // Ubah Jadi Ip Vps Mu !!!
